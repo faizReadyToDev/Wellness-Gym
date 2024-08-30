afad38f45f99d7d1086ce14cc41d5d2a8e892484
Faiz Ur Rehman <faizurrehman1415@gmail.com>
2024-08-30 20:59:53 +0530

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '400px',  // Custom extra small breakpoint
+      },
+    },
   },
   plugins: [],
 }
-
