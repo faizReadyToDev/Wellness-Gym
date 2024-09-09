6824e62559ab49e8bcb4fc6ce4f92bc8196aff8a
Faiz Ur Rehman <faizurrehman1415@gmail.com>
2024-09-09 13:27:48 +0530

    new client pics added

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -11,14 +11,14 @@ export default function Clients(){
                 <div className="item" style={{'--position': 1}}><img src="assets/client1.jpg" alt="" /></div>
                 <div className="item" style={{'--position': 2}}><img src="assets/client2.jpg" alt="" /></div>
                 <div className="item" style={{'--position': 3}}><img src="assets/client3.jpg" alt="" /></div>
-                <div className="item" style={{'--position': 4}}><img src="assets/client1.jpg" alt="" /></div>
-                <div className="item" style={{'--position': 5}}><img src="assets/client2.jpg" alt="" /></div>
-                <div className="item" style={{'--position': 6}}><img src="assets/client3.jpg" alt="" /></div>
+                <div className="item" style={{'--position': 4}}><img src="assets/client4.jpg" alt="" /></div>
+                <div className="item" style={{'--position': 5}}><img src="assets/client5.jpg" alt="" /></div>
+                <div className="item" style={{'--position': 6}}><img src="assets/client6.jpg" alt="" /></div>
                 <div className="item" style={{'--position': 7}}><img src="assets/client1.jpg" alt="" /></div>
                 <div className="item" style={{'--position': 8}}><img src="assets/client2.jpg" alt="" /></div>
-                <div className="item" style={{'--position': 9}}><img src="assets/client3.jpg" alt="" /></div>
-                <div className="item" style={{'--position': 10}}><img src="assets/client1.jpg" alt="" /></div>
-                <div className="item" style={{'--position': 11}}><img src="assets/client2.jpg" alt="" /></div>
+                <div className="item" style={{'--position': 9}}><img src="assets/client4.jpg" alt="" /></div>
+                <div className="item" style={{'--position': 10}}><img src="assets/client5.jpg" alt="" /></div>
+                <div className="item" style={{'--position': 11}}><img src="assets/client6.jpg" alt="" /></div>
                 <div className="item" style={{'--position': 12}}><img src="assets/client3.jpg" alt="" /></div>
             </div>
             <span className="before-after relative text-3xl font-extrabold bg-slate-100 text-black">Before & After</span>
